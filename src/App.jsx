c821c27d50217140479b9eb46345843de7653f5e
Lea Ellingwood <leaellingwood@hotmail.com>
2018-11-12 14:54:56 -0500

    freaking github

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
 
-// This URL can be combined with an photo id to fetch an photo.
+// This URL can be combined with a photo id to fetch a photo.
 const PHOTO_URL = "https://picsum.photos/200?photo=";
 // This URL can be used to get an array of objects that contain information
 // about various photos.
 const PHOTO_LIST_URL = "https://picsum.photos/list";
 
 class App extends Component {
-  // 1. Declare a state object that will be used to track an array of photos
   state = {
+    photos: [],
   }
-
   // 2. Declare a life cycle method
   // This life cycle method should:
-  //  - will be called after the component is initially rendered
-  // - will fetch an array of photos
-  // - will add that array of photos to state once received
+  //  - be called after the component is initially rendered
+  // - fetch an array of photos
+  // - add that array of photos to state once received
+    componentDidMount(){
+      fetch("https://picsum.photos/list")
+      .then( response => response.json())
+      // hydrates (changes it from json to readable js)
+      .then (pictures => this.setState({photos: pictures}))
+    // check setState documentation.  don't use push, etc. to change state.  change state by using setState.
+  }
+
   render() {
     const { photos = [] } = this.state;
     return (
       <React.Fragment>
         <header>
           <h1>Photo Wall</h1>
-          <p>
-            Start by reading App.jsx and completing the numbered steps.
-            Afterward, delete this paragraph. Then, open up App.css and
-            complete the instructions there.
-          </p>
         </header>
         <div className="collage">
             {/* We use map here because Array.prototype.map is an expression,
               * and for loops are not. You'll learn more about this soon! 
               */}
             {photos.map( photo => 
-                <img alt={/* 3. Fill me in with the photo's filename */ ""}
-                     key={/* 4. Fill me in with the photo's id */ ""}
-                     src={/* 5. Fill me in with the photo's URL */ ""}
+                <img id="image" alt={/* 3. Fill me in with the photo's filename */ photo.filename}
+                     key={/* 4. Fill me in with the photo's id */ photo.id}
+                     src={/* 5. Fill me in with the photo's URL */ PHOTO_URL + photo.id}
                 />
             )}
         </div>
